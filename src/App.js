38a050beb1867f41b072e81bd84d29a2c62bf999
Austin George <ageorge8000@gmail.com>
2020-08-10 22:55:23 -0400

    Iniated Landing Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styling/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="landingPrompt">you should be ...</p>
+        <button className="signinButton">signing in with google</button>
+        <button className="helpButton">wait what is this?</button>
       </header>
     </div>
   );
