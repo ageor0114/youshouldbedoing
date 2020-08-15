c3830a773834de61cb792d0c1ad985a745cb2ad1
Austin George <ageorge8000@gmail.com>
2020-08-15 15:02:39 -0400

    Updated Privacy Policy:

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -5,13 +5,12 @@ import '../styling/App.css';
 class Home extends React.Component {
 
   render(){
-    const policy = "⠀⠀Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n";
+    const policy = " to show the user's Google calendar data — so that users can get an accessible and lightweight view of their current schedule. The only data requested in this site is the name, color, start time, and end time of the user's upcoming calendar events.\n\nThis site will never add, delete, or make any changes to the user's calendar data. The calendar data requested from the user is never stored directly to this site and is never shared with any other site. It is used exclusively to display to the user for the period that they are signed in.\n\nyoushouldbedoing.com is commited to ensuring that your data is used responsibly and sparingly (only what is absolutely neccessary to display your schedule). if you’d like to reach the creator directly, you can contact me ";
     return(
       <center>
       <div className="privacy">
         <h1>Privacy Policy</h1>
-        <p>{policy}{policy}{policy}</p>
-        <p>⠀⠀If you’d like to reach the creator directly, you can contact me <a className="hyperlink" href = "mailto: a2@berkeley.edu">here</a>.</p>
+        <p>This site uses <a className="hyperlink" href = "https://www.googleapis.com/auth/calendar.events.readonly" target="_blank" rel="noopener noreferrer">https://www.googleapis.com/auth/calendar.events.readonly</a>{policy}<a className="hyperlink" href = "mailto: a2@berkeley.edu">here</a>.</p>
         <Link className="returnButton" to="/">🡠 get back to workin</Link>
       </div>
       </center>
