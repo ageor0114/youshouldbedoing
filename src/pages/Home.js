5760b95d7462cd0b0b33c0639ae839454a2b4cc7
Austin George <ageorge8000@gmail.com>
2020-08-14 20:16:31 -0400

    Reformatted Button CSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import { Redirect } from 'react-router';
 import ApiCalendar from 'react-google-calendar-api';
 import '../styling/App.css';
 
@@ -68,13 +67,6 @@ class Home extends React.Component {
     console.log("X: " + this.state.sign);
     if (this.state.sign === false) ApiCalendar.handleAuthClick();
     console.log("Y: " + this.state.sign);
-    /*if (ApiCalendar.sign)
-      ApiCalendar.listUpcomingEvents(1)
-        .then(({result}: any) => {
-          console.log(result.items);
-          console.log("XTEST: " + result.items[0].summary);
-        });
-    else ApiCalendar.handleAuthClick();*/
 
     //while(!ApiCalendar.sign) console.log("boop");
   }
@@ -88,29 +80,6 @@ class Home extends React.Component {
     console.log("redirect");
     this.setState({loggedIn:true})
   }
-
-  /*const loadClientWhenGapiReady = timer => {
-    if (timer === 0) return;
-    if (ApiCalendar.sign) {
-      if (window.location.hostname === 'localhost') {
-        getEvents().then(eventsLit => {
-          setValidEvents(true);
-          setEvents(eventsLit);
-        });
-      }
-    } else {
-      setTimeout(() => {
-        loadClientWhenGapiReady(timer - 1);
-      }, 100);
-    }
-  }
-
-  loadSingIn = () => {
-    ApiCalendar.onLoad(() => {
-      ApiCalendar.listenSign(ApiCalendar.sign);
-    });
-    loadClientWhenGapiReady(100);
-  };*/
 
   render(){
     if (/*this.state.loggedIn*/ApiCalendar.sign) {
@@ -131,28 +100,10 @@ class Home extends React.Component {
 
     return (
       <header className="App">
-        {/*
-        {validEvents && (
-          <div>
-            <p className="landingPrompt">you should be doing ...</p>
-            <h1>{this.state.event}</h1>
-          </div>
-        )}
-        {!validEvents && (
-          <div>
-          <p className="landingPrompt">you should be ...</p>
-          <button onClick={this.signin} className="signinButton">signing in with google</button>
-          <button onClick={e => handleItemClick(e, 'sign-in')}>SIGNING IN WITH GOOGLE</button>
-          <button className="helpButton">wait what is this?</button>
-          </div>
-        )}
-        */}
         <p className="prompt">you should be ...</p>
         <button onClick={this.signin} className="signinButton">signing in with google</button>
-        <button onClick={this.signout}>SignOut</button>
-        {/*<button onClick={e => handleItemClick(e, 'sign-in')}>SIGNING IN WITH GOOGLE</button>*/}
-        {/*<button onClick={this.redirect}>Redirect</button>*/}
-        <button className="helpButton">wait what is this?</button>
+        <button className="signoutButton" onClick={this.signout}>SignOut</button>
+        <button className="helpButton"><p className="helpText">wait what is this?</p></button>
       </header>
     );
   }
